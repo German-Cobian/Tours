39588f8f033438f8637ca5049933ff04dc983ec7
German Renato Cobian <germancobian@hotmail.com>
2022-02-11 12:31:41 -0800

    Implemented button to toggle between showin more/less info

--- src/Tour.js
+++ src/Tour.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 
 const Tour = ({id, image, info, price, name}) => {
+  const [readMore, setReadMore] = useState(false) // Added in this feature
+
   return (
     <article className="single-tour">
       <img src={image} alt={name} />
@@ -9,11 +11,15 @@ const Tour = ({id, image, info, price, name}) => {
           <h4>{name}</h4>
           <h4 className="tour-price">${price}</h4>
         </div>
-        <p>{info}</p>
+        <p>{readMore? info : `${info.substring(0, 200)}...`} 
+        <button onClick={() => setReadMore(! readMore)}>{readMore ? 'Show Less' : 'Read More'}</button></p>
         <button className="delete-btn">Not Interested</button>
       </footer>
     </article>
   );
 };
+
+// Added in this feature "<p>{readMore? info : `${info.substring(0, 200)}...`}" and 
+//"<button onClick={() => setReadMore(! readMore)}>{readMore ? 'Show Less' : 'Read More'}</button></p>"
 
 export default Tour;
